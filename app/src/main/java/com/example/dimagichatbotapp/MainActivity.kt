cde71135172d5f877d487996e1b9abd0768e116e
anthonynoel <antjnoel@gmail.com>
2025-09-04 14:46:37 -0300

    Updated imports

--- app/src/main/java/com/example/dimagichatbotapp/MainActivity.kt
+++ app/src/main/java/com/example/dimagichatbotapp/MainActivity.kt
@@ -5,23 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.dimagichatbotapp.ui.theme.DimagiChatbotAppTheme
-
-
-
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextField
 import java.text.SimpleDateFormat
 import java.util.Date
